b6e796253017a21bbcdc5688120c24116246481f
Luka <plucaslambert@gmail.com>
2018-05-01 08:58:03 +0300

    Main app module, did not change much to this file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {FormsModule} from "@angular/forms"
+import {HttpClientModule} from "@angular/common/http"
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
+import { GitServiceService } from './git-serv.service'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    OutputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GitServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
